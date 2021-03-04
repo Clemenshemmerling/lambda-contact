473107cde6bb79382233cfbbf106b08a6d0f2824
Clemens Hemmerling <clemens@getkor.io>
2021-03-04 14:59:19 -0600

    test

--- index.js
+++ index.js
@@ -22,11 +22,8 @@ exports.handler = async (event) => {
     html: `<b>${event.message}</b>`, // html body
   });
 
-  console.log("Message sent: %s", info.messageId);
+  return "Message sent: %s", info.messageId;
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 };
 
