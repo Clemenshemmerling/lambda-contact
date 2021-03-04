55cccdc203cc47ceb8db2d4182c6b30c3d8052c6
Clemens Hemmerling <clemens@getkor.io>
2021-03-04 14:57:45 -0600

    test

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ require('dotenv').config();
 const nodemailer = require("nodemailer");
 
 exports.handler = async (event) => {
-  return process.env.SMTP_SERVER;
-  // let transporter = nodemailer.createTransport({
-  //   host: process.env.SMTP_SERVER,
-  //   port: 587,
-  //   secure: false, // true for 465, false for other ports
-  //   auth: {
-  //     user: process.env.USER, // generated ethereal user
-  //     pass: process.env.PASSWORD, // generated ethereal password
-  //   },
-  // });
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_SERVER,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.USER, // generated ethereal user
+      pass: process.env.PASSWORD, // generated ethereal password
+    },
+  });
 
-  // // send mail with defined transport object
-  // let info = await transporter.sendMail({
-  //   from: `"👻" <${process.env.USER}>`, // sender address
-  //   to: event.email, // list of receivers
-  //   subject: event.subject, // Subject line
-  //   text: event.message, // plain text body
-  //   html: `<b>${event.message}</b>`, // html body
-  // });
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `"👻" <${process.env.USER}>`, // sender address
+    to: event.email, // list of receivers
+    subject: event.subject, // Subject line
+    text: event.message, // plain text body
+    html: `<b>${event.message}</b>`, // html body
+  });
 
-  // console.log("Message sent: %s", info.messageId);
-  // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // // Preview only available when sending through an Ethereal account
-  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 };
 
